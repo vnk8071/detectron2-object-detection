a144f4d01cd3cfe243cd4401d8c22720e8325b26
vnk8071 <nguyenkhoi8071@gmail.com>
2022-01-23 14:43:14 +0700

    ADD detectron utils

---
+++ d2detector/config.py
@@ -0,0 +1,60 @@
+from detectron2 import model_zoo
+import os
+from detectron2.config import get_cfg
+import torch
+
+
+def add_d2trainer_config(cfg):
+    """
+    Add config for d2detector
+    """
+    cfg.merge_from_file(model_zoo.get_config_file(
+        'COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml'))
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
+        'COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml')
+
+    # Input
+    cfg.INPUT.MIN_SIZE_TEST = 800
+    cfg.INPUT.MAX_SIZE_TEST = 1333
+
+    # Dataloader
+    cfg.DATALOADER.NUM_WORKERS = 2
+    cfg.DATALOADER.SAMPLER_TRAIN = "TrainingSampler"
+
+    # Model detail
+    cfg.MODEL.RESNETS.OUT_FEATURES = ["res2", "res3", "res4", "res5"]
+    # cfg.MODEL.RESNETS.DEFORM_ON_PER_STAGE = [True, True, True, True]
+    # cfg.MODEL.RESNETS.DEFORM_MODULATED = True
+    cfg.MODEL.BACKBONE.FREEZE_AT = 2
+    cfg.MODEL.ROI_BOX_HEAD.SMOOTH_L1_BETA = 0.1
+    cfg.MODEL.ROI_BOX_HEAD.NAME = "FastRCNNConvFCHead"
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
+    cfg.MODEL.ROI_HEADS.NMS_THRESH_TEST = 0.5
+    cfg.MODEL.ROI_HEADS.IN_FEATURES = ["p2", "p3", "p4", "p5"]
+    cfg.MODEL.RPN.IN_FEATURES = ["p2", "p3", "p4", "p5", "p6"]
+    # cfg.MODEL.FPN.NORM = "GN"
+
+    # Solver
+    cfg.SOLVER.IMS_PER_BATCH = 2
+    cfg.SOLVER.BASE_LR = 0.0005
+    cfg.SOLVER.MAX_ITER = 1000
+    cfg.SOLVER.STEPS = (2000,)
+    cfg.SOLVER.WARMUP_ITERS = 500
+    cfg.SOLVER.GAMMA = 0.05
+
+    cfg.TEST.EVAL_PERIOD = 1000
+    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+
+
+def add_d2test_config():
+    """
+    Add config for testing
+    """
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file(
+        "COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
+    cfg.DATALOADER.NUM_WORKERS = 2
+    cfg.SOLVER.IMS_PER_BATCH = 2
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
+    cfg.MODEL.DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+    return cfg
---
+++ d2detector/trainer.py
@@ -0,0 +1,52 @@
+import os
+from detectron2.data.transforms.augmentation import AugInput
+from detectron2.data.transforms.augmentation_impl import Resize
+from detectron2.engine import DefaultTrainer
+from detectron2.config import get_cfg
+from detectron2.data import build_detection_train_loader, build_detection_test_loader, DatasetMapper
+from detectron2.evaluation import COCOEvaluator
+import detectron2.data.transforms as T
+from detectron2.data import detection_utils as utils
+import copy
+import torch
+
+
+def build_train_aug(dataset_dict):
+    # Get image and bounding box from dataset
+    dataset_dict = copy.deepcopy(dataset_dict)
+    image = utils.read_image(dataset_dict["file_name"])
+    boxes = [obj["bbox"] for obj in dataset_dict["annotations"]]
+
+    # Data augmentation
+    transform_list = T.AugmentationList([
+        T.ResizeShortestEdge((800, ), 1200, sample_style='choice'),
+        T.RandomFlip(prob=0.8, horizontal=True),
+        T.RandomCrop(crop_type='relative', crop_size=[0.9, 0.9]),
+    ])
+    aug_input = AugInput(image, boxes=boxes)
+    transforms = transform_list(aug_input)
+    image, boxes = aug_input.image, aug_input.boxes
+    dataset_dict["image"] = torch.as_tensor(
+        image.transpose(2, 0, 1).astype("float32"))
+    annos = [
+        utils.transform_instance_annotations(obj, transforms, image.shape[:2])
+        for obj in dataset_dict["annotations"]
+        if obj.get("iscrowd", 0) == 0
+    ]
+
+    # Convert annotations to instances
+    instances = utils.annotations_to_instances(annos, image.shape[:2])
+    dataset_dict["instances"] = utils.filter_empty_instances(instances)
+    return dataset_dict
+
+
+class D2Trainer(DefaultTrainer):
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        if output_folder is None:
+            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
+        return COCOEvaluator(dataset_name, cfg, True, output_folder)
+
+    @classmethod
+    def build_train_loader(cls, cfg):
+        return build_detection_train_loader(cfg, mapper=build_train_aug)
