71403b61bf826fcdea97cd5ad0851b279f756c3e
vnk8071 <nguyenkhoi8071@gmail.com>
2022-01-23 17:40:37 +0700

    REMOVE version

--- inference.py
+++ inference.py
@@ -37,7 +37,6 @@ args = parser.parse_args()
 input_path = args.input_path
 output_path = args.output_path
 fps_predict = args.fps_predict
-version = args.version
 model = args.model_zoo
 weight = args.weight
 threshold = args.threshold
